7d76e163f20840d16fc73acc0982ec1895693600
emachipana <emachipana0102@gmail.com>
2022-09-07 18:54:55 -0500

    ADD: create user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user.as_json(except: :password_digest), status: :created
+    else
+      render json: { error: @user.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:user_type, :name, :username, :password)
+  end
+end
