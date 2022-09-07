88a94937327e4b69a2643ba5ab3186606e549d2c
emachipana <emachipana0102@gmail.com>
2022-09-07 11:51:23 -0500

    ADD: validations and asociations requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,14 @@
 class Request < ApplicationRecord
+  validates :document_type, presence: true
+  enum document_type: { 
+    dni: 0,
+    ruc: 1,
+    other: 2
+  }
+  validates :document, presence: true
+  validates :client_name, presence: true
+  validates :message, length: { maximum: 280 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_nil: true
+
+  has_many :request_details, dependent: :destroy
 end
