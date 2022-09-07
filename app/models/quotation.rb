0a93dd9886657ab1d43499fce3d511736663b166
emachipana <emachipana0102@gmail.com>
2022-09-07 11:55:14 -0500

    ADD: validations and asociations to quotations

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,2 +1,13 @@
 class Quotation < ApplicationRecord
+  validates :document_type, presence: true
+  enum document_type: { 
+    dni: 0,
+    ruc: 1,
+    other: 2
+  }
+  validates :document, presence: true
+  validates :client_name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_nil: true
+
+  has_many :quotation_details, dependent: :destroy
 end
