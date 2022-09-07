32f04cc8c7e355a50af9bf3ee58baf7889d2f4b1
emachipana <emachipana0102@gmail.com>
2022-09-07 10:40:50 -0500

    FIX: add default 0 to column user type in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_151815) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_153853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,7 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_151815) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "user_type"
+    t.integer "user_type", default: 0
     t.string "username"
     t.string "name"
     t.string "password_digest"
