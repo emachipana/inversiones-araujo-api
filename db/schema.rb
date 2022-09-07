154903055a7f88a0890f8648986d7d979fa392e6
emachipana <emachipana0102@gmail.com>
2022-09-07 11:28:29 -0500

    FIX: stock to default in zero

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_160237) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_162154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_160237) do
     t.string "name"
     t.bigint "sub_category_id", null: false
     t.bigint "user_id", null: false
-    t.integer "stock"
+    t.integer "stock", default: 0
     t.float "price"
     t.text "description"
     t.string "marca"
