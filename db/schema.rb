95582b8161a3497b3a2daaa75ff97f51de9ebd66
emachipana <emachipana0102@gmail.com>
2022-09-06 21:53:13 -0500

    ADD: sub category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_024320) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_025135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_024320) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sub_categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_sub_categories_on_category_id"
+  end
+
+  add_foreign_key "sub_categories", "categories"
 end
