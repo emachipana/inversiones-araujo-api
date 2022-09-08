1e3b4d4e17f677cab8bcd04f553ced2d0a6f35d1
emachipana <emachipana0102@gmail.com>
2022-09-08 09:49:04 -0500

    ADD: update sub category

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,4 +1,7 @@
 class SubCategoriesController < ApplicationController
+  before_action :set_sub_category, only: %i[ update ]
+  skip_before_action :authorize, only: %i[ index ]
+
   def index
     @sub_categories = SubCategory.all
     render json: @sub_categories
@@ -13,9 +16,21 @@ class SubCategoriesController < ApplicationController
     end
   end
 
+  def update
+    if @sub_category.update(sub_category_params)
+      render json: @sub_category
+    else
+      render json: { error: @sub_category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def sub_category_params
     params.permit(:category_id, :name)
+  end
+
+  def set_sub_category
+    @sub_category = SubCategory.find(params[:id])
   end
 end
