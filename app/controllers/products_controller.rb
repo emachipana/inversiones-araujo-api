2220b52414dffca65dc7d01aa87953bbfe47b53d
emachipana <emachipana0102@gmail.com>
2022-09-08 12:01:09 -0500

    ADD: create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,29 @@ class ProductsController < ApplicationController
   
   def index
     @products = Product.all
-    @products = @products.map { |product|
-      product.as_json.merge({
-        sub_category_name: product.sub_category.name,
-        category_name: product.sub_category.category.name
-      })
-    }
+    @products = @products.map { |product| product.as_json.merge(extract_categories_name(product)) }
     render json: @products
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+     render json: @product.as_json.merge(extract_categories_name(@product)) 
+    else
+      render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.permit(:name, :sub_category_id, :stock, :price, :description, :marca, :unit_metric)
+  end
+
+  def extract_categories_name(product)
+    {
+      sub_category_name: product.sub_category.name,
+      category_name: product.sub_category.category.name
+    }
   end
 end
