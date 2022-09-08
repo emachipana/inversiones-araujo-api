47da9134c8716bc1d8482846b6f57abd5006aa1c
emachipana <emachipana0102@gmail.com>
2022-09-08 09:42:29 -0500

    ADD: create sub category

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,2 +1,16 @@
 class SubCategoriesController < ApplicationController
+  def create
+    @sub_category = SubCategory.new(sub_category_params)
+    if @sub_category.save
+      render json: @sub_category, status: :created
+    else
+      render json: { error: @sub_category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sub_category_params
+    params.permit(:category_id, :name)
+  end
 end
