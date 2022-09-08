40ff66e16aa0ec2afd6e504e3a48188027a57fd6
emachipana <emachipana0102@gmail.com>
2022-09-07 22:00:54 -0500

    ADD: update user & method set category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[update]
+
   def index
     @categories = Category.all
     render json: @categories
@@ -11,5 +13,19 @@ class CategoriesController < ApplicationController
     else
       render json: { error: @category.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @category.update(params[:name])
+      render json: @category
+    else
+      render json: { error: @category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
