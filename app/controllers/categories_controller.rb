96611685928ae29eea73f24d340064d7289da90d
emachipana <emachipana0102@gmail.com>
2022-09-07 21:56:22 -0500

    ADD: create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,13 @@ class CategoriesController < ApplicationController
     @categories = Category.all
     render json: @categories
   end
+
+  def create
+    @category = Category.new(name: params[:name])
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: { error: @category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
