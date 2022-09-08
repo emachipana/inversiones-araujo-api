9f3e371e42bb4a5d7f4e3c826925f6aff4cfd629
emachipana <emachipana0102@gmail.com>
2022-09-08 11:14:56 -0500

    ADD: index products with sub category name & category name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+  skip_before_action :authorize, only: %i[ index ]
+  
+  def index
+    @products = Product.all
+    @products = @products.map { |product|
+      product.as_json.merge({
+        sub_category_name: product.sub_category.name,
+        category_name: product.sub_category.category.name
+      })
+    }
+    render json: @products
+  end
 end
