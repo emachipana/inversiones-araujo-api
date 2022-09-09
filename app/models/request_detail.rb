617895a30402a98e0f112506cf9579ef1010aa65
emachipana <emachipana0102@gmail.com>
2022-09-09 09:45:45 -0500

    ADD: validate to product id and request id are unique

--- app/models/request_detail.rb
+++ app/models/request_detail.rb
@@ -1,5 +1,6 @@
 class RequestDetail < ApplicationRecord
   validates :quantity, presence: true
+  validates_uniqueness_of :product_id, scope: :request_id
 
   belongs_to :product
   belongs_to :request
