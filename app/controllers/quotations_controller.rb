68fe2b2c3326cf96107c05f265633df47b0f7e91
emachipana <emachipana0102@gmail.com>
2022-09-09 18:05:26 -0500

    ADD: create quotation

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -11,6 +11,40 @@ class QuotationsController < ApplicationController
     render json: merge_items(@quotation)
   end
 
+  def create
+    @quotation = Quotation.new(quotation_params)
+    if !params[:items] || params[:items].size.zero?
+      render json: { error: [ "Items is required" ] }, status: :unprocessable_entity
+    else
+      if @quotation.save
+        record = []
+        params[:items].each do |item|
+          product = Product.find(item["product_id"]) 
+    
+          @quotation_detail = QuotationDetail.new(
+            quotation: @quotation,
+            product: product,
+            quantity: item["quantity"],
+            subtotal: product.price * item["quantity"]
+          )
+
+          if @quotation_detail.save then record.push(true) else record.push(false) end
+        end
+
+        if record.find { |el| el == false }.nil?
+          total = @quotation.quotation_details.map{ |el| el.subtotal }.reduce(:+)
+          @quotation.update(total: total)
+          render json: merge_items(@quotation)
+        else
+          @quotation.destroy
+          render json: { error: [ "Something was wrong" ] }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: @quotation.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
+
   private
 
   def merge_items(quotation)
@@ -19,5 +53,9 @@ class QuotationsController < ApplicationController
 
   def set_quotation
     @quotation = Quotation.find(params[:id])
+  end
+
+  def quotation_params
+    params.permit(:document_type, :document, :client_name, :email, :phone, :address, :total)
   end
 end
