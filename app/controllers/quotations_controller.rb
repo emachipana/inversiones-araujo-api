9065f3bf1da70963c792563fe7b98a5eef542ed8
emachipana <emachipana0102@gmail.com>
2022-09-09 10:05:49 -0500

    ADD: show quotation

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,13 +1,23 @@
 class QuotationsController < ApplicationController
+  before_action :set_quotation, only: %i[ show ]
+
   def index
-    @quotations = Request.all
+    @quotations = Quotation.all
     @quotations = @quotations.map { |quotation| merge_items(quotation) }
     render json: @quotations
+  end
+
+  def show
+    render json: merge_items(@quotation)
   end
 
   private
 
   def merge_items(quotation)
     quotation.as_json.merge(items: quotation.quotation_details)
+  end
+
+  def set_quotation
+    @quotation = Quotation.find(params[:id])
   end
 end
