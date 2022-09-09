326763695a51a515376c12dc78fbfbd21b5ed1af
emachipana <emachipana0102@gmail.com>
2022-09-09 09:57:10 -0500

    ADD: index quotations

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,2 +1,13 @@
 class QuotationsController < ApplicationController
+  def index
+    @quotations = Request.all
+    @quotations = @quotations.map { |quotation| merge_items(quotation) }
+    render json: @quotations
+  end
+
+  private
+
+  def merge_items(quotation)
+    quotation.as_json.merge(items: quotation.quotation_details)
+  end
 end
