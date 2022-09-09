9af57b470f8fd3be0dbc7edc5fa940577bbdb205
emachipana <emachipana0102@gmail.com>
2022-09-09 18:05:56 -0500

    ADD: validation in model

--- app/models/quotation_detail.rb
+++ app/models/quotation_detail.rb
@@ -1,6 +1,7 @@
 class QuotationDetail < ApplicationRecord
   validates :quantity, presence: true
   validates :subtotal, presence: true
+  validates_uniqueness_of :product_id, scope: :quotation_id
 
   belongs_to :product
   belongs_to :quotation
