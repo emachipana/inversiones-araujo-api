83f155bda1aa227b559ebebc9bba235f0f2dea75
emachipana <emachipana0102@gmail.com>
2022-09-10 12:44:38 -0500

    ADD: seeds to create users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying all categories"
+Category.destroy_all
+
+puts "Destroying all Quotations"
+Quotation.destroy_all
+
+puts "Destroying all Requests"
+Request.destroy_all
+
+puts "Destoying all Users"
+User.destroy_all
+
+# create users
+puts "Create users"
+users = [
+  { 
+    name: "Mariano Chipana Contreras",
+    username: "admin1",
+    password: "123456",
+    user_type: 1
+  },
+  { 
+    name: "Yurfa Araujo Estrada",
+    username: "admin",
+    password: "123456",
+    user_type: 1
+  },
+  {
+    name: "Testino Di Prueba",
+    username: "client-test",
+    password: "letmein"
+  }
+]
+
+users.each do |user|
+  user = User.new(user)
+  if user.save
+    puts "User was created successfully"
+  else
+    puts user.errors.full_messages.join(" ")
+  end
+end
