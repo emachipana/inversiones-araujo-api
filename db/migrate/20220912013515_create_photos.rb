6060e495b01b3bff63b5d9dc77e0c29edc17887f
emachipana <emachipana0102@gmail.com>
2022-09-11 20:36:40 -0500

    ADD: photo model

---
+++ db/migrate/20220912013515_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :photos do |t|
+      t.string :public_id
+      t.string :url
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
