3cf456dc6ca85f089b0056e05d3ea2d66edd60c9
emachipana <emachipana0102@gmail.com>
2022-09-12 17:36:11 -0500

    FIX: origins for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    origins "http://localhost:3001, http://localhost:3000"
 
-    resource "http://localhost:3000/",
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
