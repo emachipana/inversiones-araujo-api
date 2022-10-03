e03794654ada4c04fd96742a21013fb82fe2513e
emachipana <emachipana0102@gmail.com>
2022-10-03 11:22:13 -0500

    FIX: remove public id column

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,4 @@
 class Photo < ApplicationRecord
-  validates :public_id, presence: true, uniqueness: true
   validates :url, presence: true
   
   belongs_to :product
--- db/migrate/20220912013515_create_photos.rb
+++ db/migrate/20220912013515_create_photos.rb
@@ -1,12 +1,10 @@
 class CreatePhotos < ActiveRecord::Migration[7.0]
   def change
     create_table :photos do |t|
-      t.string :public_id
       t.string :url
       t.references :product, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :photos, :public_id, unique: true
   end
 end
