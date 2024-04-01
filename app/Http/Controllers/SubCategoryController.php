76ddfe64c7ab902f9924ceeeddb90e89d3796604
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-01 18:21:01 -0500

    ADD: sub category model

---
+++ app/Http/Controllers/SubCategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubCategoryRequest;
+use App\Http\Requests\UpdateSubCategoryRequest;
+use App\Models\SubCategory;
+
+class SubCategoryController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+      //
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+      //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreSubCategoryRequest $request)
+  {
+      //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(SubCategory $subCategory)
+  {
+      //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(SubCategory $subCategory)
+  {
+      //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateSubCategoryRequest $request, SubCategory $subCategory)
+  {
+      //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(SubCategory $subCategory)
+  {
+      //
+  }
+}
