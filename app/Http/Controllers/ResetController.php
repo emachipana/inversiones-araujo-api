a30ac451a1472b0e87463f31020c4e232a304291
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-01 19:24:55 -0500

    ADD: reset model

---
+++ app/Http/Controllers/ResetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreResetRequest;
+use App\Http\Requests\UpdateResetRequest;
+use App\Models\Reset;
+
+class ResetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreResetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reset $reset)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Reset $reset)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateResetRequest $request, Reset $reset)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reset $reset)
+    {
+        //
+    }
+}
