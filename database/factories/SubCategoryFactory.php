f4c2d7b30153c075fc3dd6bbeb930026a54d067c
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-02 22:49:46 -0500

    ADD: categories factories

--- database/factories/SubCategoryFactory.php
+++ database/factories/SubCategoryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class SubCategoryFactory extends Factory
   public function definition(): array
   {
     return [
-        //
+      "category_id" => Category::factory(),
+      "name" => $this->faker->word()
     ];
   }
 }
