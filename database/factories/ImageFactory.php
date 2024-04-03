3136c0b676ebca1bedc7bd2b7cbda4a9df15df12
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-03 01:08:09 -0500

    ADD: image factory

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -9,15 +9,20 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ImageFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      "url" => $this->faker->randomElement([
+        "https://http2.mlstatic.com/D_NQ_NP_703223-MLC32168119697_092019-O.webp",
+        "https://hagalo.mx/23210-large_default/tijera-para-podar-curva-8-pulgadas-truper-18453.jpg",
+        "https://http2.mlstatic.com/D_NQ_NP_851478-MLA54911942504_042023-O.webp",
+        "https://hydroenv.com.mx/catalogo/images/00/soluciones_y_nutrientes/fertilizantes/Fertilizantes/cab.jpg"
+      ])
+    ];
+  }
 }
