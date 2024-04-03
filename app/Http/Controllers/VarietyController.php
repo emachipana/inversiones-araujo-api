b9b82d867dfa44689d86f9fc4a6e9f635249b351
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-03 11:32:44 -0500

    FIX: add variety model with migration

---
+++ app/Http/Controllers/VarietyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVarietyRequest;
+use App\Http\Requests\UpdateVarietyRequest;
+use App\Models\Variety;
+
+class VarietyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVarietyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Variety $variety)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Variety $variety)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVarietyRequest $request, Variety $variety)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Variety $variety)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_04_02_000060_create_varieties_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('varieties', function (Blueprint $table) {
+      $table->id();
+      $table->string("name");
+      $table->float("price");
+      $table->unsignedBigInteger("tuber_id");
+      $table->timestamps();
+
+      $table->foreign("tuber_id")->references("id")->on("tubers");
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('varieties');
+  }
+};
