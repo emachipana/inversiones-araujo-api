f8363da69a0958496553dc3772ac07960fc5d9a8
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-03 00:52:09 -0500

    ADD: product factory

--- database/factories/DiscountFactory.php
+++ database/factories/DiscountFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class DiscountFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      "product_id" => Product::factory(),
+      "price" => $this->faker->randomFloat(2, 5, 99),
+      "percentage" => $this->faker->numberBetween(10, 99)
+    ];
+  }
 }
