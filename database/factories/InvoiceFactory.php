b82cb555507dae59b2c9f30a7de5d43e905b3b96
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-03 01:37:23 -0500

    ADD: invoice factory

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -9,15 +9,24 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class InvoiceFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      "invoice_type" => $this->faker->randomElement(["factura", "boleta"]),
+      "document_type" => $this->faker->randomElement(["dni", "ruc"]),
+      "document" => $this->faker->randomNumber(8),
+      "currency_type" => $this->faker->randomElement(["sol", "dolar americano"]),
+      "first_name" => $this->faker->name(),
+      "last_name" => $this->faker->lastName(),
+      "issue_date" => $this->faker->dateTimeThisYear(),
+      "due_date" => $this->faker->dateTimeThisYear(),
+      "pdf_url" => $this->faker->url(),
+      "address" => $this->faker->streetAddress()
+    ];
+  }
 }
