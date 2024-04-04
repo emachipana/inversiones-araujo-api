83da9c1e42dfd80136dea088df5b9fb7e40bd6dc
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-04 10:07:12 -0500

    ADD: message factory

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -9,15 +9,21 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class MessageFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      "first_name" => $this->faker->name(),
+      "last_name" => $this->faker->lastName(),
+      "phone" => $this->faker->randomNumber(9),
+      "subject" => $this->faker->sentence(),
+      "content" => $this->faker->text(),
+      "origin" => $this->faker->randomElement(["agroinvitro", "inversiones_araujo"]),
+      "email" => $this->faker->email()
+    ];
+  }
 }
