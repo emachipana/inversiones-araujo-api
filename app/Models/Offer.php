b2c51c3d3c7eb0e859fc29c1271adb40c505b8d4
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-04 23:05:00 -0500

    ADD: offer relations

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = [
+    "title",
+    "sub_title",
+    "is_used"
+  ];
+
+  public function offerProducts() {
+    return $this->hasMany(OfferProduct::class);
+  }
 }
