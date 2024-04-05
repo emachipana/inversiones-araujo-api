eb120fc5816448917124287057a3542feefdfcd6
emachipana <emachipana0102@gmail.com>
2024-04-05 18:21:39 -0500

    ADD: dicount seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
   {
     $this->call([
       CategorySeeder::class,
-      ProductSeeder::class
+      ProductSeeder::class,
+      DiscountSeeder::class
     ]);
   }
 }
--- database/seeders/DiscountSeeder.php
+++ database/seeders/DiscountSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Discount;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DiscountSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $products = Product::all();
+
+    for($i = 1; $i <= 10; $i++) {
+      $product = $products->random();
+
+      Discount::factory()->create([
+        "product_id" => $product->id
+      ]);
     }
+  }
 }
