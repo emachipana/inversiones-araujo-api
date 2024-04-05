30dd084444969dfb8b5a8fd7e8582ca1f0a8e5de
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-04 22:16:36 -0500

    ADD: product and categories relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
   use HasFactory;
+
+  protected $fillable = [
+    "name"
+  ];
+
+  public function subCategories() {
+    return $this->hasMany(SubCategory::class);
+  }
+
+  public function products() {
+    return $this->hasMany(Product::class);
+  }
 }
--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class SubCategory extends Model
 {
   use HasFactory;
+
+  protected $fillable = [
+    "category_id",
+    "name"
+  ];
+
+  public function category() {
+    return $this->belongsTo(Category::class);
+  }
+
+  public function products() {
+    return $this->hasMany(Product::class);
+  }
 }
