0b2f6b300d894a56f2314daecc1046b4d7aa2a11
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-06 11:55:46 -0500

    ADD: client seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ClientSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    Client::factory()->count(15)->create();
+  }
 }
