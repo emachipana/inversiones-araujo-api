b125ee5dd981b3c1a2ea739f8e49d4b68c21c7ca
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-10 12:14:49 -0500

    ADD: index and show image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,63 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreImageRequest;
 use App\Http\Requests\UpdateImageRequest;
+use App\Http\Resources\ImageCollection;
+use App\Http\Resources\ImageResource;
 use App\Models\Image;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return new ImageCollection(Image::all());
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreImageRequest $request)
+  {
+  
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreImageRequest $request)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Image $image)
+  {
+    return new ImageResource($image);
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Image $image)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateImageRequest $request, Image $image)
+  {
+      //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Image $image)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateImageRequest $request, Image $image)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Image $image)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Image $image)
+  {
+      //
+  }
 }
