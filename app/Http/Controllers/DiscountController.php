b64eab57bf12e238515edc97733b5d67502ad954
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-10 11:52:13 -0500

    ADD: create, update and destroy discount

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -4,63 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreDiscountRequest;
 use App\Http\Requests\UpdateDiscountRequest;
+use App\Http\Resources\DiscountResource;
 use App\Models\Discount;
+use App\Models\Product;
 
 class DiscountController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+{ 
+  protected function discountPercentage($product_id, $price) {
+    $product = Product::findOrFail($product_id);
+    $percentage = ($product->price - $price) / $product->price * 100;
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    return ceil($percentage);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDiscountRequest $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreDiscountRequest $request)
+  {
+    $data = $request->all();
+    $product_id = $data["product_id"];
+    $price = $data["price"];
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Discount $discount)
-    {
-        //
-    }
+    $percentage = $this->discountPercentage($product_id, $price);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Discount $discount)
-    {
-        //
-    }
+    return new DiscountResource(Discount::create([
+      "product_id" => $product_id,
+      "price" => $price,
+      "percentage" => ceil($percentage)
+    ]));
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateDiscountRequest $request, Discount $discount)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateDiscountRequest $request, Discount $discount)
+  {
+    $data = $request->all();
+    $price = $data["price"];
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Discount $discount)
-    {
-        //
-    }
+    $percentage = $this->discountPercentage($discount->product->id, $price);
+
+    $discount->update([
+      "price" => $price,
+      "percentage" => $percentage
+    ]);
+
+    return new DiscountResource($discount);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Discount $discount)
+  {
+    $discount->delete();
+  }
 }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -24,6 +24,7 @@ class ProductResource extends JsonResource
       "category_id" => $this->category_id,
       "sub_category_id" => $this->sub_category_id,
       "images" => ProductImageResource::collection($this->whenLoaded("productImages")),
+      "discount" => $this->discount,
       "created_at" => $this->created_at,
       "updated_at" => $this->updated_at
     ];
