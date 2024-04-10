5a1a3a540735651f44b5310059705ce02f332961
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-10 12:53:32 -0500

    ADD: store, update and destroy product image

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -4,63 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductImageRequest;
 use App\Http\Requests\UpdateProductImageRequest;
+use App\Http\Resources\ProductImageResource;
 use App\Models\ProductImage;
 
 class ProductImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreProductImageRequest $request)
+  {
+    return new ProductImageResource(ProductImage::create($request->all()));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateProductImageRequest $request, ProductImage $productImage)
+  {
+    $productImage->update($request->all());
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreProductImageRequest $request)
-    {
-        //
-    }
+    return new ProductImageResource($productImage);
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(ProductImage $productImage)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ProductImage $productImage)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateProductImageRequest $request, ProductImage $productImage)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(ProductImage $productImage)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(ProductImage $productImage)
+  {
+    $productImage->delete();
+  }
 }
--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -15,6 +15,7 @@ class ProductImageResource extends JsonResource
   public function toArray(Request $request): array
   {
     return [
+      "id" => $this->id,
       "product_id" => $this->product_id,
       "image_id" => $this->image_id,
       "image_url" => $this->image->url,
