1c5dd3ccc16a176c27234d462f9ae5c07be06a5e
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-18 21:04:56 -0500

    ADD: store update and destroy order product

--- app/Http/Controllers/OrderProductController.php
+++ app/Http/Controllers/OrderProductController.php
@@ -4,63 +4,61 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderProductRequest;
 use App\Http\Requests\UpdateOrderProductRequest;
+use App\Http\Resources\OrderProductResource;
 use App\Models\OrderProduct;
+use App\Models\Product;
 
 class OrderProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreOrderProductRequest $request)
+  {
+    $data = $request->all();
+    $product = Product::findOrFail($data["product_id"]);
+
+    if($product->discount) {
+      $sub_total = $data["quantity"] * $product->discount->price;
+    }else {
+      $sub_total = $data["quantity"] * $product->price;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    return new OrderProductResource(OrderProduct::create([
+      "product_id" => $data["product_id"],
+      "order_id" => $data["order_id"],
+      "quantity" => $data["quantity"],
+      "sub_total" => $sub_total,
+      "product_name" => $product->name
+    ]));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateOrderProductRequest $request, OrderProduct $orderProduct)
+  {
+    $data = $request->all();
+
+    if($orderProduct->product->discount) {
+      $sub_total = $data["quantity"] * $orderProduct->product->discount->price;
+    }else {
+      $sub_total = $data["quantity"] * $orderProduct->product->price;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreOrderProductRequest $request)
-    {
-        //
-    }
+    $orderProduct->update([
+      "quantity" => $data["quantity"],
+      "sub_total" => $sub_total
+    ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(OrderProduct $orderProduct)
-    {
-        //
-    }
+    return new OrderProductResource($orderProduct);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(OrderProduct $orderProduct)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateOrderProductRequest $request, OrderProduct $orderProduct)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(OrderProduct $orderProduct)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(OrderProduct $orderProduct)
+  {
+    $orderProduct->delete();
+  }
 }
