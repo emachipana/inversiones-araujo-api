4a14f731e21cb95d71d20c650e75abd97f0ee408
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-26 10:19:21 -0500

    FIX: show product on offer product resource

--- app/Http/Resources/OfferProductResource.php
+++ app/Http/Resources/OfferProductResource.php
@@ -14,6 +14,12 @@ class OfferProductResource extends JsonResource
    */
   public function toArray(Request $request): array
   {
-    return parent::toArray($request);
+    return [
+      "id" => $this->id,
+      "offer_id" => $this->offer_id,
+      "product" => new ProductResource($this->product->loadMissing("productImages")),
+      "created_at" => $this->created_at,
+      "updated_at" => $this->updated_at
+    ];
   }
 }
