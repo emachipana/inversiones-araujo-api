04d1c65c1b8e4a081a037bdb285ce16d572b3511
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-30 10:53:23 -0500

    ADD: invoice CRUD

--- app/Http/Requests/StoreInvoiceRequest.php
+++ app/Http/Requests/StoreInvoiceRequest.php
@@ -3,26 +3,37 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreInvoiceRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
+  public function rules(): array
+  {
+    return [
+      "invoice_type" => ["required", Rule::in(["boleta", "factura"])],
+      "document_type" => ["required", Rule::in(["dni", "ruc"])],
+      "document" => ["required", "numeric"],
+      "currency_type" => ["required", "min:3"],
+      "first_name" => ["required", "min:3"],
+      "last_name" => ["required", "sometimes", "min:3"],
+      "address" => ["required", "min:3"],
+      "issue_date" => ["required", "date"],
+      "due_date" => ["required", "sometimes", "date"],
+      "comment" => ["required", "sometimes", "min:3"],
+      "pdf_url" => ["required"]
+    ];
+  }
 }
